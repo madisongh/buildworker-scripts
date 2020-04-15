93620be015a757108688485151a9a4dcd88469c4
Matt Madison <matt@madison.systems>
2020-04-15 09:56:46 -0700

    Fix up autobuilder leftovers.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-BUILDBOTVERSION = '2.7.0'
-
 setup(
     name='buildworker-scripts',
     version='0.1.0',
@@ -9,14 +7,11 @@ setup(
     license='MIT',
     author='Matt Madison',
     author_email='matt@madison.systems',
+    url='https://github.com/madisongh/buildworker-scripts',
     entry_points={
         'console_scripts': [
-            'store-artifacts = autobuilder.scripts.store_artifacts:main',
+            'store-artifacts = buildworker_scripts.scripts.store_artifacts:main',
         ]
-    },
-    include_package_data=True,
-    package_data={
-        'autobuilder': ['templates/*.txt']
     },
     install_requires=['aws-secretsmanager-caching',
                       'boto3', 'botocore']
