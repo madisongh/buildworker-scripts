6372c34e30214a8a4d9396731966e96d2cebcf74
Matt Madison <matt@madison.systems>
2022-01-24 05:12:58 -0800

    Switch to using setup.cfg and pyproject.toml
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -1,18 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='buildworker-scripts',
-    version='0.1.0',
-    packages=find_packages(),
-    license='MIT',
-    author='Matt Madison',
-    author_email='matt@madison.systems',
-    url='https://github.com/madisongh/buildworker-scripts',
-    entry_points={
-        'console_scripts': [
-            'store-artifacts = buildworker_scripts.scripts.store_artifacts:main',
-        ]
-    },
-    install_requires=['aws-secretsmanager-caching',
-                      'boto3', 'botocore']
-)
+from setuptools import setup
+setup()
